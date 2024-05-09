bc98b496bba2312ebc43e5ac2c8f3ff78082bd6b
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-05-09 09:48:31 +0700

    feat (auth-visitor): implement register and login

--- src/app/(visitor)/auth/register/page.tsx
+++ src/app/(visitor)/auth/register/page.tsx
@@ -9,23 +9,23 @@ import { Label } from "@/components/ui/label"
 import { supabaseServer } from "@/lib/supabase/server"
 import { useRouter } from "next/navigation"
 
-export default function AuthLogin() {
+export default function AuthRegister() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState(null)
   const router = useRouter()
 
-  const handleLogin = async () => {
+  const handleRegister = async () => {
     try {
-      const { data, error } = await supabaseServer.auth.signInWithPassword({
+      const { data, error } = await supabaseServer.auth.signUp({
         email: email,
         password: password,
       })
       if (error) {
         throw error
       }
-      console.log("Login successful:", data)
-      router.push("/")
+      console.log("Registration successful:", data)
+      router.push("/auth/login")
     } catch (error: any) {
       setError(error.message)
       console.error("Registration error:", error.message)
@@ -37,13 +37,13 @@ export default function AuthLogin() {
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
+            <h1 className="text-3xl font-bold">Register</h1>
             <p className="text-balance text-muted-foreground">Enter your email below to login to your account</p>
           </div>
           <div className="grid gap-4">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="m@example.com" required onChange={(e) => setEmail(e.target.value)} />
+              <Input id="email" type="email" placeholder="m@example.com" onChange={(e) => setEmail(e.target.value)} required />
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
@@ -55,17 +55,17 @@ export default function AuthLogin() {
               <Input id="password" type="password" required onChange={(e) => setPassword(e.target.value)} />
             </div>
             {error && <p className="text-red-500">{error}</p>}
-            <Button type="submit" className="w-full" onClick={handleLogin}>
-              Login
+            <Button type="submit" className="w-full" onClick={handleRegister}>
+              Register
             </Button>
             <Button variant="outline" className="w-full">
-              Login with Google
+              Register with Google
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
-            <Link href="/auth/register" className="underline">
-              Register
+            <Link href="/auth/login" className="underline">
+              Login
             </Link>
           </div>
         </div>
