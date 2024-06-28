4fb13c150f51f300b96d05c6d4b02d1e66883709
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-06-28 10:02:01 +0700

    fix(about): space x and y

--- src/components/layout/about-section.tsx
+++ src/components/layout/about-section.tsx
@@ -4,9 +4,9 @@ export default function AboutSection() {
   return (
     <div className="bg-muted/40 dark:bg-muted/20 px-10 py-6 rounded-md" id="about-section">
       <p className="text-center text-2xl font-semibold mb-5">About me</p>
-      <div className="grid md:grid-cols-10 space-x-5 lg:space-x-5">
-        <div className="col-span-2">
-          <Image src={"/rizky.jpg"} alt="Profile" width={500} height={500} className="w-60 h-full lg:w-full rounded-md lg:rounded-e-sm object-cover lg:h-full" />
+      <div className="grid grid-cols-1 md:grid-cols-10 max-[768px]:space-y-5 md:space-x-5">
+        <div className="col-span-1 md:col-span-2">
+          <Image src={"/rizky.jpg"} alt="Profile" width={500} height={500} className="h-96 md:h-full w-full rounded-md lg:rounded-e-sm object-cover lg:h-full" />
         </div>
         <div className="col-span-1 md:col-span-8 rounded-md">
           I am a developer with a passion for building scalable and maintainable application and website. I have experience in building web applications using React and etc. I am also interested in building mobile applications using
--- src/components/layout/career-section.tsx
+++ src/components/layout/career-section.tsx
@@ -53,9 +53,21 @@ export default function CareerSection() {
         }
       >
         <MagicCard className="flex w-full cursor-pointer flex-col items-center justify-center overflow-hidden p-20 shadow-2xl">
-          <p className="z-10 whitespace-nowrap text-4xl font-medium text-gray-800 dark:text-gray-200">
-            Magic
-          </p>
+        <p className="text-lg font-semibold mb-2">Work Experience</p>
+          <ol className="relative border-s border-gray-200 dark:border-gray-700">
+            <li className="mb-10 ms-4">
+              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time>
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
+              <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+            </li>
+            <li className="mb-10 ms-4">
+              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March 2022</time>
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Marketing UI design in Figma</h3>
+              <p className="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+            </li>
+          </ol>
           <div className="pointer-events-none absolute inset-0 h-full bg-[radial-gradient(circle_at_50%_120%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
         </MagicCard>
         <MagicCard className="flex w-full cursor-pointer flex-col items-center justify-center overflow-hidden p-20 shadow-2xl">
