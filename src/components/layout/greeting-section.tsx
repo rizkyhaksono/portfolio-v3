420c502cbab91e3c0e0b1e49ca0ad9996b2d162b
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-08-22 22:03:47 +0700

    feat(about): wip

--- src/components/layout/greeting-section.tsx
+++ src/components/layout/greeting-section.tsx
@@ -7,11 +7,11 @@ import { useEffect, useState } from "react";
 export default function GreetingSection() {
   const { theme } = useTheme();
   const [color, setColor] = useState("#ffffff");
- 
+
   useEffect(() => {
     setColor((theme === "dark" || theme === "system") ? "#ffffff" : "#000000");
   }, [theme]);
- 
+
   return (
     <div className="relative flex h-screen w-full items-center justify-center overflow-hidden rounded-lg">
       <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-7xl font-semibold leading-none text-transparent dark:from-white dark:to-black md:text-9xl">
