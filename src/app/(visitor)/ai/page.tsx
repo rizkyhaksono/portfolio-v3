c3140e124a0fc4ff16a96fcd37c2cce199b64ad8
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-12-03 22:40:45 +0700

    fix(ai): using mdx

--- src/app/(visitor)/ai/page.tsx
+++ src/app/(visitor)/ai/page.tsx
@@ -2,6 +2,7 @@ import BlurFade from "@/components/magicui/blur-fade";
 import { getAIData } from "@/services/visitor/ai";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import MDXComponent from "@/components/ui/mdx-components";
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
@@ -10,30 +11,12 @@ export default async function AIPage(props: Readonly<{
 }>) {
   const searchParams = await props.searchParams;
   const query = Array.isArray(searchParams.query) ? searchParams.query[0] : searchParams.query ?? "Hello";
-
-  let response;
-  try {
-    response = await getAIData(query);
-  } catch (error) {
-    console.error("Error fetching AI data:", error);
-    response = null;
-  }
-
-  const candidates = response?.response?.candidates ?? [];
-  console.log(candidates);
-  const formattedResponses = candidates.map((candidate) =>
-    candidate?.content?.parts
-      ?.map((part) => part?.text ?? "")
-      .join(" ")
-      .replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>")
-      .replace(/\n/g, "<br />")
-  );
-  console.log(formattedResponses);
+  const response = await getAIData(query);
 
   return (
     <BlurFade delay={0.25} inView>
       <>
-        <h1 className="text-lg font-bold">{"Natee AI 😁 [Working on Progress]"}</h1>
+        <h1 className="text-lg font-bold">{"Natee AI ⚡"}</h1>
         <form method="GET" action="/ai" className="flex flex-1 items-center mt-2 gap-2">
           <Input
             type="text"
@@ -45,21 +28,14 @@ export default async function AIPage(props: Readonly<{
         </form>
 
         <div className="mt-4">
-          {candidates.length > 0 ? (
-            candidates.map((candidate, index) => (
-              <div key={candidate?.avgLogprobs ?? index} className="mb-4 p-4 border rounded shadow">
-                <p>
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: formattedResponses[index],
-                    }}
-                  />
-                </p>
-              </div>
-            ))
-          ) : (
-            <p className="text-gray-500">No candidates found. Please try another query.</p>
-          )}
+          {response.response.candidates.map((candidate, index) => (
+            <div
+              key={candidate.avgLogprobs}
+              className="bg-secondary p-4 rounded-lg shadow-md mt-4"
+            >
+              <MDXComponent>{candidate.content.parts.map((part) => part.text).join(" ")}</MDXComponent>
+            </div>
+          ))}
         </div>
       </>
     </BlurFade>
