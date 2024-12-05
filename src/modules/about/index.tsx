9155410a4de03cc9163c9b625f804ede3595a7c1
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-12-05 18:25:05 +0700

    feat(about): add target to link

--- src/modules/about/index.tsx
+++ src/modules/about/index.tsx
@@ -11,7 +11,7 @@ export default function AboutSection() {
         <div className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
           Experience in Software Development with skills in Web and Mobile Development. I am still an undergraduate and have experience related to web and mobile development as well as UI/UX design.
           <div className="w-fit">
-            <Link href="https://drive.google.com/file/d/1Rzeaa292L_wESKSW8FonMaXus9Hv5xSG/view?usp=sharing">
+            <Link href="https://drive.google.com/file/d/1Rzeaa292L_wESKSW8FonMaXus9Hv5xSG/view?usp=sharing" target="_blank">
               <Button className="flex gap-2 mt-4 rounded-sm" variant={"default"} size={"sm"}>
                 CV <Download className="size-4" />
               </Button>
