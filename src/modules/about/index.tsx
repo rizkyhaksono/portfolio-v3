ef83d42ad5d11dad5441560f6b8f74b4581625c6
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-12-05 12:11:06 +0700

    feat(about): change to drive

--- src/modules/about/index.tsx
+++ src/modules/about/index.tsx
@@ -1,6 +1,7 @@
 import BlurFade from "@/components/magicui/blur-fade";
 import { Button } from "@/components/ui/button";
 import { Download } from "lucide-react";
+import Link from "next/link";
 
 export default function AboutSection() {
   return (
@@ -10,11 +11,11 @@ export default function AboutSection() {
         <div className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
           Experience in Software Development with skills in Web and Mobile Development. I am still an undergraduate and have experience related to web and mobile development as well as UI/UX design.
           <div className="w-fit">
-            <a href="/cv-rizky-v3.pdf" download>
+            <Link href="https://drive.google.com/file/d/1Rzeaa292L_wESKSW8FonMaXus9Hv5xSG/view?usp=sharing">
               <Button className="flex gap-2 mt-4 rounded-sm" variant={"default"} size={"sm"}>
                 CV <Download className="size-4" />
               </Button>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
