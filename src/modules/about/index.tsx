8910bb84db932822ffa78fe1511919b3a73d2529
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-12-30 00:26:13 +0700

    imp(about): update cv and portfolio links

--- src/modules/about/index.tsx
+++ src/modules/about/index.tsx
@@ -1,6 +1,6 @@
 import BlurFade from "@/components/magicui/blur-fade";
 import { Button } from "@/components/ui/button";
-import { Download } from "lucide-react";
+import { ExternalLink, FileText } from "lucide-react";
 import Link from "next/link";
 
 export default function AboutSection() {
@@ -8,12 +8,17 @@ export default function AboutSection() {
     <BlurFade delay={0.25} inView>
       <div className="mt-10">
         <p className="text-left text-xl font-semibold">About</p>
-        <div className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+        <div className="font-sans text-sm text-muted-foreground dark:prose-invert">
           Experience in Software Development with skills in Web and Mobile Development. I am still an undergraduate and have experience related to web and mobile development as well as UI/UX design.
-          <div className="w-fit">
-            <Link href="https://drive.google.com/file/d/1Rzeaa292L_wESKSW8FonMaXus9Hv5xSG/view?usp=sharing" target="_blank">
-              <Button className="flex gap-2 mt-4 rounded-sm" variant={"default"} size={"sm"}>
-                CV <Download className="size-4" />
+          <div className="flex gap-4 w-full mt-4">
+            <Link href="https://drive.google.com/file/d/1OXy50uSiyhbMBRzVyBstMW7-kAwQ37VZ/view?usp=sharing" target="_blank">
+              <Button variant={"outline"} size={"sm"}>
+                View CV <ExternalLink size={16} />
+              </Button>
+            </Link>
+            <Link href="https://drive.google.com/file/d/1xb9nKFHr05NksJC5AAB0gl1fNco2apyP/view?usp=sharing" target="_blank">
+              <Button variant={"outline"} size={"sm"}>
+                View Portfolio in PDF <FileText size={16} />
               </Button>
             </Link>
           </div>
