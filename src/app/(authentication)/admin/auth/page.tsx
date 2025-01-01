75e6383427a02aebb28ed0167ab7c72c00b0bb2e
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-01-02 05:47:30 +0700

    fix(admin): auth url and style card

--- src/app/(authentication)/admin/auth/page.tsx
+++ src/app/(authentication)/admin/auth/page.tsx
@@ -32,7 +32,7 @@ export default function AdminLoginPage() {
   };
 
   return (
-    <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 max-[766px]:mt-40">
+    <div className="container flex h-screen items-center justify-center relative flex-col md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-zinc-900"></div>
         <Link className="relative z-20 flex items-center text-lg font-medium" href="/">
