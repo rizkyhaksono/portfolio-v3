db97ce302b488677f47e7a2502511487373039e3
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-07-04 03:32:57 +0700

    feat(layout): enable smooth cursor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
           <Toaster position="top-right" />
-          {/* <SmoothCursor /> */}
+          <SmoothCursor />
           <Analytics />
         </ThemeProvider>
       </body>
