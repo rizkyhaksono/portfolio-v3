c125a1f05a93a0db16e541179c01441297cab57f
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-09-15 14:44:45 +0700

    feat(config): remote pattern allow all hostname

--- next.config.mjs
+++ next.config.mjs
@@ -4,80 +4,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.nightcafe.studio",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "infotech.umm.ac.id",
-      },
-      {
-        protocol: "https",
-        hostname: "venturo.id",
-      },
-      {
-        protocol: "https",
-        hostname: "encrypted-tbn0.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.bigio.id",
-      },
-      {
-        protocol: "https",
-        hostname: "www.bejana.id",
-      },
-      {
-        protocol: "https",
-        hostname: "media.licdn.com",
-      },
-      {
-        protocol: "https",
-        hostname: "mpqpeetocugibrrsnsyp.supabase.co",
-      },
-      {
-        protocol: "https",
-        hostname: "media.dev.to",
-      },
-      {
-        protocol: "https",
-        hostname: "media2.dev.to",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "dev-to-uploads.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i.pinimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "dicoding-assets.sgp1.cdn.digitaloceanspaces.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.google.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.icon-icons.com",
-      },
-      {
-        protocol: "https",
-        hostname: "icons.veryicon.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn-images-1.medium.com",
-      },
+        hostname: "*",
+      }
     ],
   },
 }
