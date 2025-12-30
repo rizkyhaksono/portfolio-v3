ab324856fffd0a8c71407e06a02619ae57dea7ac
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-12-30 16:04:28 +0700

    chore(components): enhance codes and layout

--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -70,7 +70,7 @@ export const InfiniteMovingCards = ({
             className="relative w-[350px] max-w-full shrink-0 rounded-2xl border border-b-0 border-zinc-200 bg-[linear-gradient(180deg,#fafafa,#f5f5f5)] px-8 py-6 md:w-[450px] dark:border-zinc-700 dark:bg-[linear-gradient(180deg,#27272a,#18181b)]"
             key={`${item.name}-${idx}`}
           >
-            <blockquote>
+            <blockquote className="flex flex-col justify-between h-full">
               <div aria-hidden="true" className="user-select-none pointer-events-none absolute -left-0.5 -top-0.5 -z-1 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"></div>
               <span className="relative z-20 text-sm font-normal leading-[1.6] text-neutral-800 dark:text-gray-100">{item.quote}</span>
               <div className="relative z-20 mt-6 flex flex-row items-center gap-3">
